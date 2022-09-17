a4de57fc5d23a607588908b6b92e9ed22f413120
rafael_.moura <rafael.moura.dev.29@gmail.com>
2022-09-17 16:06:41 -0300

    🐛

--- app/backend/src/exceptions/DbError.ts
+++ app/backend/src/exceptions/DbError.ts
@@ -1,4 +1,5 @@
 import ApiError from "./ApiError";
+import CODE_MESSAGES_FUNCTION from "../config/CodeMessagesFunction";
 import { CODE_MESSAGES } from "../config/CodeMessages";
 import { ICodeMessageFunction, ICodeMessage } from "../Interfaces/ICodeMessage";
 import HTTP_STATUS_CODES from "../config/httpStatusCode";
@@ -14,7 +15,7 @@ export class DBError extends ApiError.ApiErrorMother {
     let status_code: number = HTTP_STATUS_CODES.BAD_REQUEST;
     switch (error.code) {
       case 'ER_DUP_ENTRY':
-        error_message_function = CODE_MESSAGES.ALREADY_EXIST_DATA as ICodeMessageFunction;
+        error_message_function = CODE_MESSAGES_FUNCTION.ALREADY_EXIST_DATA;
         code_message = error_message_function(db_table_object_name);
         break;
 
